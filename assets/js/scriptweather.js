142d7987b4000f8261ce680ce0f7868b1479c930
Sung Min An <an.sungmin17@icloud.com>
2024-09-17 22:02:46 -0700

    Progress so far weatherjs

--- assets/js/scriptweather.js
+++ assets/js/scriptweather.js
@@ -3,15 +3,31 @@
 // How to get api out of the form & connect to front page form (connect with currency, country, etc -- 4 pieces of info)
 // Connect to currency api & put info the page
 // Change current weather for 5 day (?) & icons
+// string name city country stringify
 
-const clicky = document.getElementById('weatherdataicon');
-const weatherResultEl = document.getElementById('weatherdataicon');
-const weatherCityEl = document.getElementById('weatherdataicon')
+const weatherResultEl = document.getElementById("weatherdataicon");
+const weatherCityEl = document.getElementById("weatherdataicon");
+
+// Function that will take the input values from city and country *Still go to figure it out*
+function inputCCs() {
+  // make a loop for the city and country until it is filled correctly
+  const inputCity = document.getElementById("weatherdataicon").value;
+  const inputCountry = document.getElementById("weatherdataicon").value;
+  const inputCityStr = JSON.stringify(inputCity);
+  const inputCountryStr = JSON.stringify(inputCountry);
+  // Figure ouy how to get the inputs before the loop below
+  inputCity.input();
+  inputCountry.input();
+  // Need to get both inputs before starting the loop or it will break
+  while (!city || !country) {
+    console.log(`Please fill in both spaces.`);
+  }
+}
+
 // Getting parameters for the city and country
-function getWeather(city,country) {
+function getWeather(city, country) {
   // fetch request gets a list of all the repos for the node.js organization
-  const requestUrlGeocode = 
-  `http://api.openweathermap.org/geo/1.0/direct?q=${city},${country}&limit=1&appid=6bdabfafea6c9fb1c11b7b85ca98c4ca`
+  const requestUrlGeocode = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${country}&limit=1&appid=6bdabfafea6c9fb1c11b7b85ca98c4ca`;
 
   // Fetch for first response
   fetch(requestUrlGeocode)
@@ -22,26 +38,34 @@ function getWeather(city,country) {
     .then(function (data) {
       console.log(data[0]);
       const resultObj = data[0];
-      let lat = resultObj.lat
-      let long = resultObj.lon
+      let lat = resultObj.lat;
+      let long = resultObj.lon;
       // Getting the lat and long of the city, country stated in the parameter in the first fetch function
-      const requestUrlWeather =
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=6bdabfafea6c9fb1c11b7b85ca98c4ca`;
+      // *Changing it for the 5 day instead of current* current: current
+      const requestUrlWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=6bdabfafea6c9fb1c11b7b85ca98c4ca`;
       // Fetch geocode from location now (from the weather)
       fetch(requestUrlWeather)
-      .then(function(response) {
-        return response.json();
-      })
-      // Looking to change from Current Weather to 5 day weather api
-      .then(function(weatherdata) {
-        console.log(weatherdata.weather[0]);
-        // "Decorative" part
-        const weatherObj = weatherdata.weather[0];
-        const iconWeather = document.createElement('img');
-        iconWeather.setAttribute('src',`https://openweathermap.org/img/wn/${weatherObj.icon}@2x.png`)
-        weatherResultEl.appendChild(iconWeather);
-      })
-  });
+        .then(function (response) {
+          return response.json();
+        })
+        // Looking to change from Current Weather to 5 day weather api
+        .then(function (weatherdata) {
+          console.log(weatherdata.weather[0]);
+          // "Decorative" part
+          const weatherObj = weatherdata.weather[0];
+          const iconWeather = document.createElement("img");
+          iconWeather.setAttribute(
+            "src",
+            `https://openweathermap.org/img/wn/${weatherObj.icon}@2x.png`
+          );
+          weatherResultEl.appendChild(iconWeather);
+
+          // Icon for city
+          const iconCity = document.createElement("img");
+          iconCity.setAttribute("src", "./assets/images/weather2.jpg");
+          weatherCityEl.appendChild(iconCity);
+        });
+    });
 }
 
-getWeather('New York', 'US')
+getWeather(inputCCs);
